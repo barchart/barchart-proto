a26bb9e71d79978233f7e1dacb5da327836ebba1
Joel Dudley <Joel.Dudley@barchart.com>
2012-08-24 10:04:49 -0500

    a

--- translators/common-translator/src/test/java/com/barchart/translator/common/fields/OffsetCalculatorTest.java
+++ translators/common-translator/src/test/java/com/barchart/translator/common/fields/OffsetCalculatorTest.java
@@ -6,7 +6,7 @@ public class OffsetCalculatorTest {
 
 	@Test
 	public void testOffsetCalculator() {
-		OffsetCalculator calculator = new OffsetCalculator();
+		OffsetCalculator calculator = new OffsetCalculator(null);
 		
 		FixedLengthField futuresSymbol = calculator.newField(6);
 		FixedLengthField reserved1 = calculator.newField(1);
@@ -17,6 +17,10 @@ public class OffsetCalculatorTest {
 	}
 	
 	private static class Fields extends OffsetCalculator {
+		public Fields(Codec codec) {
+			super(codec);
+			// TODO Auto-generated constructor stub
+		}
 		public final StringField futuresSymbol = super.newStringField(6);
 		public final StringField reserved1 = super.newStringField(1);
 		public final IntegerField bidQuote = super.intField(8); 
@@ -24,7 +28,7 @@ public class OffsetCalculatorTest {
 	
 	private static final class Message {
 		
-		private static final Fields fields = new Fields();
+		private static final Fields fields = new Fields(null);
 		
 		public Message() {
 		}
