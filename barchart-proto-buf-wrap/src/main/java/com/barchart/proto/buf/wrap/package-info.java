c8b2c5b4ac5861bae45fdd376ab2116b536b1437
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2012-09-19 12:28:35 -0500

    packet wrapper

---
+++ barchart-proto-buf-wrap/src/main/java/com/barchart/proto/buf/wrap/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2011-2012 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package com.barchart.proto.buf.wrap;
+
+/**
+ *
+ * public api for management of proto.buf messages
+ * 
+ * see tests for examples
+ * 
+ */
