c248e940505587e1e96793356714facfe96aad86
Joel Dudley <Joel.Dudley@barchart.com>
2012-11-21 13:54:34 -0600

    Add descriprots and marketentrycollapser

---
+++ barchart-proto-buf-wrap/src/main/java/com/barchart/proto/buf/wrap/PacketWrapperFile.java
@@ -0,0 +1,87 @@
+package com.barchart.proto.buf.wrap;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.zip.GZIPInputStream;
+
+public class PacketWrapperFile implements Iterable<PacketWrapper> {
+
+	private final File file;
+
+	public PacketWrapperFile(File file) {
+		this.file = file;
+	}
+
+	@Override
+	public Iterator<PacketWrapper> iterator() {
+		try {
+			return new IteratorImpl(getInputStreamForPacketFile());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private InputStream getInputStreamForPacketFile() throws IOException {
+		InputStream fileInputStream = new FileInputStream(file);
+		if (file.getName().endsWith(".gz")) {
+			return new GZIPInputStream(fileInputStream);
+		} else {
+			return fileInputStream;
+		}
+	}
+
+	private static class IteratorImpl implements Iterator<PacketWrapper> {
+
+		private final InputStream inputStream;
+		private PacketWrapper next;
+
+		public IteratorImpl(InputStream inputStream) throws IOException {
+			this.inputStream = inputStream;
+			this.next = readNext();
+
+		}
+
+		private PacketWrapper readNext() {
+			try {
+				PacketWrapper nextWrapper = PacketWrapper.parseDelimitedFrom(inputStream);
+				if (nextWrapper == null) {
+					inputStream.close();
+				}
+				return nextWrapper;
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public boolean hasNext() {
+			return next != null;
+		}
+
+		@Override
+		public PacketWrapper next() {
+			PacketWrapper wrapper = next;
+			next = readNext();
+			return wrapper;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "PacketWrapperFile [file=" + file + "]";
+	}
+
+	public String getName() {
+		return file.getName();
+	}
+
+}
